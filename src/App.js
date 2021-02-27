5ea4c7faab9b5d0bfdc92f18235a22ec169ead28
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-27 09:18:49 +0600

    added dynamic event handler and cart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
    const [countries, setCountries] =useState([]);
+   const [cart,setCart] = useState([]);
+
    useEffect(()=>{
      fetch('https://restcountries.eu/rest/v2/all')
      .then(res=>res.json())
@@ -12,12 +15,18 @@ function App() {
      .catch(error => console.log(error))
    },[])
 
+   const handleAddCountry = (country) => {
+     const newCart = [...cart, country];
+     setCart(newCart);
+   };
   return (
     <div className="App">
       <h1>Country Loaded : {countries.length}</h1>
+      <h4>Country Added : {cart.length}</h4>
+      <Cart cart={cart}></Cart>
       
         {
-          countries.map(country => <Country country={country} key = {country.alpha3Code}></Country>)
+          countries.map(country => <Country country={country} handleAddCountry={handleAddCountry} key = {country.alpha3Code}></Country>)
         }
       
       <header className="App-header">
