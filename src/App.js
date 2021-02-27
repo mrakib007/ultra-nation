ce05d685e6c48f72a2cbcaee8c61ed1ad5d8aadf
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-02-27 08:30:58 +0600

    ultra nation with react (basics)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country/Country';
 
 function App() {
+   const [countries, setCountries] =useState([]);
+   useEffect(()=>{
+     fetch('https://restcountries.eu/rest/v2/all')
+     .then(res=>res.json())
+     .then(data=>setCountries(data))
+     .catch(error => console.log(error))
+   },[])
+
   return (
     <div className="App">
+      <h1>Country Loaded : {countries.length}</h1>
+      
+        {
+          countries.map(country => <Country country={country} key = {country.alpha3Code}></Country>)
+        }
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Country/Country.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Country = (props) => {
+    const {name,population,region,flag} = props.country;
+    const flagStyle = {height:'50px'};
+    return (
+        <div>
+            <h4>This is {name}</h4>
+            <img style={flagStyle} src={flag} alt=""/>
+            <p>Population : {population}</p>
+            <p><small>Region: {region}</small></p>
+        </div>
+    );
+};
+
+export default Country;
